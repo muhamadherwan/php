340076336eed4354ce4bfbbd3c594ebc474c4f3c
muhamadherwan <hello.wanlab@gmail.com>
2022-04-16 01:04:30 +0800

    delete product

--- crud/index.php
+++ crud/index.php
@@ -60,7 +60,12 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
             <td><?php echo $product['create_date'] ?></td>
             <td>
                 <button type="button" class="btn btn-sm btn-outline-primary">Edit</button> 
-                <button type="button" class="btn btn-sm btn-outline-danger">Delete</button>
+
+                <form action="delete.php" method="post" style="display:inline-block">
+                    <input type="hidden" name="id" value="<?php echo $product['id'] ?>">
+                    <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                </form>
+                
             </td>
         </tr>
     <?php } ?>
