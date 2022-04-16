5a2cd73448b3ea9cd3489800e07100dc68b63a03
muhamadherwan <hello.wanlab@gmail.com>
2022-04-16 15:28:02 +0800

    install composer and guzzle to make autoloding and namespace

---
+++ autoloding/index.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'vendor/autoload.php';
+
+use app\Email;
+use app\Person;
+
+$email = new Email();
+$person = new Person();
+
+// $client = new \GuzzleHttp\Client();
+// $response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+
+// echo $response->getStatusCode(); // 200
+// echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+// echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
