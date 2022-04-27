4fcba31d38bebef5c2810fec22363850ffc06f2d
muhamadherwan <hello.wanlab@gmail.com>
2022-04-27 16:13:18 +0800

    constructor, destructor, setter method and getter methods

--- phpbook/oop/oop1/class3.php
+++ phpbook/oop/oop1/class3.php
@@ -4,17 +4,44 @@ class Person {
     
     // properties
     public $name;
+    private $eyeColor;
+    private $age;
+
+    // constructors
+    public function __construct( $name, $eyeColor, $age) {
+        $this->name = $name;
+        $this->eyeColor = $eyeColor;
+        $this->age = $age;
+    }
 
     // methods 
     public function setName($name):string {
         $this->name = $name;
         return $this->name;
     }
+
+    // eye color setter methods
+    public function setEyeColor($color) {
+        $this->eyeColor = $color;
+    }
+
+    // eye color getter methods
+    public function getEyeColor() {
+        return $this->eyeColor;
+    }
 }
 
 
-$laravel = new Person;
-echo $laravel->setName('taylor otwell').PHP_EOL;
+$laravel = new Person('taylor otwell', 'blue', 41);
+echo $laravel->setName('TO').PHP_EOL;
 
-$ror = new Person;
-echo $ror->setName('DHH').PHP_EOL;
+$laravel->setEyeColor('red');
+echo $laravel->getEyeColor().PHP_EOL;
+// var_dump($laravel);
+
+
+// $ror = new Person('DHH', 'pink', 44);
+// echo $ror->setName('David').PHP_EOL;
+
+// echo $laravel->name.PHP_EOL;
+// var_dump($ror);
