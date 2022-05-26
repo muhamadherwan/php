abb4af27168d6bc2365eeb4299ef7f1521e39c3b
Muhamad Herwan <hello.wanlab@gmail.com>
2022-05-26 11:56:42 +0800

    Update database.php

--- crud2/database.php
+++ crud2/database.php
@@ -1,6 +1,6 @@
 <?php
 
 // connect to database
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root', 'wasd1234');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root', 'root');
 // if cant connect, throw error
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
