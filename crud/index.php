e0cdcb693a73d82f3d49f9b60517bd52e01432b2
Muhamad Herwan <hello.wanlab@gmail.com>
2022-05-26 11:58:10 +0800

    Update index.php

--- crud/index.php
+++ crud/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // connect to database
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root', 'wasd1234');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root', 'root');
 // if cant connect, throw error
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
